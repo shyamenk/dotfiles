567dab6230840386eb47c12cd3e15930e7997450
Shyam Kumar <syamkumar.vm@cybersapient.io>
2024-07-25 11:46:28 +0530

    remove regolith

--- .config/nvim/lua/shyam/plugins/lualine.lua
+++ .config/nvim/lua/shyam/plugins/lualine.lua
@@ -3,61 +3,74 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 		"folke/noice.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
 	},
-
 	config = function()
-		require("lualine").setup({
+		local lualine = require("lualine")
+		-- local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+		local colors = {
+			blue = "#89b4fa",
+			green = "#a6e3a1",
+			violet = "#cba6f7",
+			yellow = "#f9e2af",
+			red = "#f38ba8",
+			fg = "#cdd6f4",
+			bg = "#313244",
+			inactive_bg = "#313244",
+			seperator = "#45475a",
+		}
+
+		local my_lualine_theme = {
+			normal = {
+				a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.seperator, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			insert = {
+				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.seperator, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			visual = {
+				a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.seperator, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			command = {
+				a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.seperator, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			replace = {
+				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+				b = { bg = colors.seperator, fg = colors.fg },
+				c = { bg = colors.bg, fg = colors.fg },
+			},
+			inactive = {
+				a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+				b = { bg = colors.seperator, fg = colors.semilightgray },
+				c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+			},
+		}
+
+		-- configure lualine with modified theme
+		lualine.setup({
 			options = {
+				theme = my_lualine_theme,
 				icons_enabled = true,
-				theme = "catppuccin",
+				-- theme = "catppuccin",
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
-				},
-				sections = {
-					lualine_z = {
-						function()
-							return " " .. os.date("%-I:%02M %p")
-						end,
-					},
-				},
 
-				ignore_focus = {},
-				always_divide_middle = true,
-				globalstatus = false,
-				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
-				},
+				globalstatus = true,
 			},
+
 			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
+				lualine_x = { "filetype" },
+				lualine_b = { "branch" },
 			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = {},
 		})
 	end,
 }
--- .config/nvim/lua/shyam/plugins/zen-mode.lua
+++ .config/nvim/lua/shyam/plugins/zen-mode.lua
@@ -3,26 +3,32 @@ return {
 	"tpope/vim-obsession",
 	config = function()
 		require("zen-mode").setup({
-			window = {
-				backdrop = 0.95,
-				width = 120,
-				height = 1,
-				options = {
-					signcolumn = "no",
-					number = false,
-					relativenumber = false,
-				},
-			},
+			vim.keymap.set("n", "<leader>gz", "<cmd>ZenMode<cr>", { desc = "Toggle Zen Mode" }),
+			-- window = {
+			-- 	backdrop = 0.95,
+			-- 	width = 120,
+			-- 	height = 1,
+			-- 	options = {
+			-- 		signcolumn = "no",
+			-- 		number = false,
+			-- 		relativenumber = false,
+			-- 	},
+			-- },
+			options = {},
 			plugins = {
-				options = {
-					enabled = true,
-					ruler = true,
-					showcmd = false,
-					laststatus = 0,
-				},
-				twilight = { enabled = false },
-				gitsigns = { enabled = false },
-				tmux = { enabled = true },
+				-- 	options = {
+				-- 		enabled = true,
+				-- 		ruler = true,
+				-- 		showcmd = false,
+				-- 		laststatus = 0,
+				-- 	},
+				-- 	twilight = { enabled = false },
+				-- 	gitsigns = { enabled = false },
+				tmux = { enabled = false },
+				-- 	alacritty = {
+				-- 		enabled = true,
+				-- 		font = "18", -- font size
+				-- 	},
 			},
 		})
 	end,
