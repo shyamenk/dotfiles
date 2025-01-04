483ae0ea6b7d74c6887629317af6249284ceb501
syamkumar <syamkumar.vm@cyversapient.io>
2025-01-04 22:51:45 +0530

    final

--- .config/nvim/lua/shyam/plugins/lsp/mason.lua
+++ .config/nvim/lua/shyam/plugins/lsp/mason.lua
@@ -3,11 +3,13 @@ return {
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"jose-elias-alvarez/null-ls.nvim", -- Add null-ls for formatting
 	},
 	config = function()
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
 		local mason_tool_installer = require("mason-tool-installer")
+		local null_ls = require("null-ls")
 
 		mason.setup({
 			ui = {
@@ -24,7 +26,6 @@ return {
 				"html",
 				"cssls",
 				"tailwindcss",
-				"svelte",
 				"lua_ls",
 				"graphql",
 				"emmet_ls",
