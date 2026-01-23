fd85d62dd94d952b70b19b207861a352c0e40edd
Shyam <shyamenk@gmail.com>
2026-01-23 16:27:44 +0530

    Update Hyprland config: fix workspace rules syntax for v0.53+, restore blur settings

--- .config/hypr/scripts/wallpaper-rotate.sh
+++ .config/hypr/scripts/wallpaper-rotate.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Auto-rotate wallpapers every 5 minutes
+# Rotate to a random wallpaper
+# Run on startup or manually to change wallpaper
 
 WALLPAPER_DIR="/home/shyamenk/Pictures/wallpaper"
-INTERVAL=300  # 5 minutes
 
-while true; do
-    WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" \) | shuf -n 1)
+# Find a random wallpaper
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" \) | shuf -n 1)
+
+if [ -n "$WALLPAPER" ]; then
     swww img "$WALLPAPER" --transition-type fade --transition-duration 2
-    sleep $INTERVAL
-done
+fi
